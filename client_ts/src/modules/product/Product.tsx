5f9d8caa337a43ce7719b71d7487bea3319964d1
adnankhayrou <adnanekhayrou3@gmail.com>
2024-01-30 16:35:28 +0100

    adding navbar and cart icon with count

--- client_ts/src/modules/product/Product.tsx
+++ client_ts/src/modules/product/Product.tsx
@@ -1,20 +1,23 @@
 import { useContext } from "react";
 import { DataContext } from "../../context/DataProvider"
 import { ProductItems } from "../../AppInterface";
+import NavBar from "../layouts/NavBar";
+
 
 function product() {
     const {topRated, allCategories, productsNearYou} = useContext(DataContext);
   return <>
   {/* navBar */}
-  <h1 className="text-3xl font-bold underline">
+  <NavBar/>
+  <h1 className="text-3xl mb-10 font-bold underline">
       Hello world!
     </h1>
 
   <div className="wrapper">
-      <div className="content">
+      <div className="content ms-5 ">
         <div className="container mb-5">
           <h4 className="my-4 font-semibold text-blue-600">Top rated</h4>
-          <div className="flex">
+          <div className="flex flex-wrap">
             {topRated.map((items: ProductItems, idx: number) => (
                 <div key={idx} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
                 <a href="#">
@@ -24,7 +27,7 @@ function product() {
                     
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{items.name}</h5>
                    
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Price : {items.price}DH</p>
                     <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
@@ -36,7 +39,7 @@ function product() {
             </div>
           <h4 className="my-4 font-semibold text-blue-600">All Categories</h4>
          
-          <div className="flex">
+          <div className="flex flex-wrap">
             {allCategories.map((items: ProductItems, idx: number) => (
                 <div key={idx} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
                 <a href="#">
@@ -46,7 +49,7 @@ function product() {
                    
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{items.name}</h5>
                     
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Price : {items.price}DH</p>
                     <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
@@ -58,7 +61,7 @@ function product() {
             ))}
             </div>
           <h4 className="my-4 font-semibold text-blue-600">Dishes Near You</h4>
-          <div className="flex">
+          <div className="flex flex-wrap">
             {productsNearYou.map((items: ProductItems, idx: number) => (
                 <div key={idx} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
                 <a href="#">
@@ -68,7 +71,7 @@ function product() {
                    
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{items.name}</h5>
                 
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Price : {items.price}DH</p>
                     <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
