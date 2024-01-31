240b4d27a5571620f1a895bbe9c176a1ef27ecef
adnankhayrou <adnanekhayrou3@gmail.com>
2024-01-31 14:55:16 +0100

    adding add to cart function and remove item from cart function

--- client_ts/src/context/DataProvider.tsx
+++ client_ts/src/context/DataProvider.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import {ProductItems} from "../AppInterface"
-export interface AppContext{
+
+interface AppState {
     topRated: ProductItems[];
     allCategories: ProductItems[];
     productsNearYou: ProductItems[];
+    cartItemCount: number;
+    cartItems: ProductItems[];
 }
 
-export interface AppState extends AppContext{
-
+interface AppContext extends AppState {
+    addToCart: (item: ProductItems) => void;
+    removeItem: (item: ProductItems) => void;
 }
 
 export const DataContext = React.createContext<AppContext>({} as AppContext);
@@ -16,21 +20,21 @@ function DataProvider({children}:{children: JSX.Element}) {
   const [state, setState] = useState<AppState>({
     topRated: [
         {
-            id:"652ebe78352060965955z231",
+            id:"652ebe78352060965955z278",
             name: "iphone case",
             image: "src/assets/image1.jpg",
             price: 20,
             quantity: 50
         },
         {
-            id:"652ebe78352060965955z231",
+            id:"652ebe78352060965955z211",
             name: "phone holder",
             image: "src/assets/image2.jpg",
             price: 80,
             quantity: 20
         },
         {
-            id:"652ebe78352060965955z231",
+            id:"652ebe78352060965955z232",
             name: "iphone case",
             image: "src/assets/image4.jpg",
             price: 30,
@@ -39,21 +43,21 @@ function DataProvider({children}:{children: JSX.Element}) {
     ],
     allCategories: [
         {
-            id:"652ebe78352060965955z231",
+            id:"652ebe78352060965955z210",
             name: "iphone case",
             image: "src/assets/image1.jpg",
             price: 20,
             quantity: 50
         },
         {
-            id:"652ebe78352060965955z231",
+            id:"652ebe78352060965955z299",
             name: "phone holder",
             image: "src/assets/image2.jpg",
             price: 80,
             quantity: 20
         },
         {
-            id:"652ebe78352060965955z231",
+            id:"652ebe78352060965955z209",
             name: "iphone case",
             image: "src/assets/image4.jpg",
             price: 30,
@@ -62,31 +66,99 @@ function DataProvider({children}:{children: JSX.Element}) {
     ],
     productsNearYou: [
         {
-            id:"652ebe78352060965955z231",
+            id:"652ebe78352060965955z255",
             name: "iphone case",
             image: "src/assets/image1.jpg",
             price: 20,
             quantity: 50
         },
         {
-            id:"652ebe78352060965955z231",
+            id:"652ebe78352060965955z238",
             name: "phone holder",
             image: "src/assets/image2.jpg",
             price: 80,
             quantity: 20
         },
         {
-            id:"652ebe78352060965955z231",
+            id:"652ebe78352060965955z277",
             name: "iphone case",
             image: "src/assets/image4.jpg",
             price: 30,
             quantity: 40
         }
     ],
+    cartItemCount: 0,
+    cartItems: [],
   })
-  const { topRated, allCategories, productsNearYou } = state
+  const { topRated, allCategories, productsNearYou, cartItemCount, cartItems } = state;
+
+  const addToCart = (item: ProductItems) => {
+    console.log(item);
+    const data = { ...item, quantity: 1 };
+    if (cartItems.length > 0) {
+      //  2 cases
+      const bool = cartItems.some((el) => el.id === item.id);
+      if (bool) {
+        const itemIndex = cartItems.findIndex((el) => el.id === item.id);
+        const c = [...state.cartItems];
+        if (c[itemIndex]["quantity"]) {
+          c[itemIndex]["quantity"]! += 1;
+        }
+        setState((prevState) => {
+          return { ...prevState, cartItems: c };
+        });
+      } else {
+        setState((prevState) => {
+          return { ...prevState, cartItems: [...state.cartItems, data] };
+        });
+      }
+    } else {
+      setState((prevState) => {
+        return { ...prevState, cartItems: [...state.cartItems, data] };
+      });
+    }
+    setState((prevState) => {
+      return { ...prevState, cartItemCount: state.cartItemCount + 1 };
+    });
+  };
+
+  const removeItem = (item: ProductItems) => {
+    if (cartItems.length > 0) {
+      let bool = state.cartItems.some((i) => i.id === item.id);
+      if (bool) {
+        let itemIndex = state.cartItems.findIndex((el) => el.id === item.id);
+        const c = [...state.cartItems];
+        // if qty > 1 then reduce by 1 else we will be splicing
+        if (cartItems[itemIndex]["quantity"] === 1) {
+          c.splice(itemIndex, 1);
+          setState((prevState) => {
+            return { ...prevState, cartItems: c };
+          });
+        } else {
+          c[itemIndex]["quantity"]! -= 1;
+          setState((prevState) => {
+            return { ...prevState, cartItems: c };
+          });
+        }
+        if (cartItemCount !== 0) {
+          setState((prevState) => {
+            return { ...prevState, cartItemCount: state.cartItemCount - 1 };
+          });
+        }
+      }
+    }
+  };
+
   return (
-  <DataContext.Provider value={{topRated, allCategories, productsNearYou}}>
+  <DataContext.Provider value={{
+    topRated, 
+    allCategories, 
+    productsNearYou,
+    cartItemCount,
+    cartItems,
+    addToCart,
+    removeItem,
+    }}>
     {children}
     </DataContext.Provider>
   );
--- client_ts/src/modules/cart/Cart.tsx
+++ client_ts/src/modules/cart/Cart.tsx
@@ -1,8 +1,121 @@
+import { useContext } from "react";
+import NavBar from "../layouts/NavBar";
+import { DataContext } from "../../context/DataProvider";
+import { ProductItems } from "../../AppInterface";
 
 function cart() {
+  const { cartItems, addToCart, removeItem } = useContext(DataContext);
+
+  const totalPrice = () => {
+    let price = 0;
+    cartItems.forEach((elment) => {
+      price += elment.quantity! * elment.price;
+    });
+    return price;
+  };
+
   return (
-    <div>cart</div>
+    <>
+    <NavBar/>
+    <div className="flex flex-col cart-outer-div">
+      <div className="flex-1 cart-body">
+        {totalPrice() ? (
+          <CartWithItems
+            cartItems={cartItems}
+            addToCart={addToCart}
+            removeItem={removeItem}
+            totalPrice={totalPrice()}
+          />
+        ) : (
+          <div className="container my-5 text-center">
+            <img src="src/assets/cart.png" className="w-48 mx-auto mt-44" alt="icon" />
+            <div className="mt-4 me-20">
+              <h4 className="text-orange-red font-semibold">Your cart is empty</h4>
+              <h5 className="text-darkblue font-semibold">
+                You can go to the home page to view all Product items.
+              </h5>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+    </>
   )
 }
+
+const CartWithItems = ({
+  cartItems,
+  addToCart,
+  removeItem,
+  totalPrice,
+}: {
+  cartItems: ProductItems[];
+  addToCart: (item: ProductItems) => void;
+  removeItem: (item: ProductItems) => void;
+  totalPrice: number;
+}) => {
+
+  return (
+    <div className="container mb-5">
+      <h4 className="my-4 my-cart text-orange-500 font-semibold">My Cart</h4>
+      <div className="flex my-3 justify-between">
+        <h4 className="font-semibold">Summary</h4>
+        <h4 className="font-semibold mr-48">Cart</h4>
+      </div>
+
+      {/* Left Section */}
+      <div className="flex">
+        <div className="grid grid-cols-2 w-1/2">
+          <div className="col-span-1">
+            <h6>Total:</h6>
+            <h6>Promo Code:</h6>
+            <h6>Shipping:</h6>
+            <div className="my-3 line w-full"></div>
+            <h6>Subtotal:</h6>
+          </div>
+          <div className="col-span-1">
+            <h6>{totalPrice}DH</h6>
+            <h6>MOBITECH</h6>
+            <h6>50DH</h6>
+            <div className="my-3 line w-60"></div>
+            <h6>{totalPrice + 50}DH</h6>
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div className="w-1/2">
+          <ul className="p-0">
+            {cartItems?.map((item: ProductItems, idx: number) => (
+              <li key={idx} className="list-none">
+                <div className="flex items-center justify-between cart-items p-4 bg-white">
+                  <img src={item.image} width="50px" height="50px" className="rounded-full" alt="icon" />
+                  <h6 className="mt-4">{item.name}</h6>
+                  <div className="flex mt-4">
+                    <button
+                      className="add-remove-btn"
+                      type="button"
+                      onClick={() => removeItem(item)}>
+                      -
+                    </button>
+                    <span className="cart-quantity mx-1">{item.quantity}</span>
+                    <button
+                      className="add-remove-btn"
+                      type="button"
+                      onClick={() => addToCart(item)}>
+                      +
+                    </button>
+                  </div>
+                  <p className="mt-4">Total Price</p>
+                  <h6 className="mt-4">{item.quantity! * item.price}DH</h6>
+                </div>
+                <div className="line"></div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default cart
