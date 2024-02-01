4856c2f54996edb358d89fc282a44f67b6e03df5
adnankhayrou <adnanekhayrou3@gmail.com>
2024-02-01 13:51:48 +0100

    adding cart section ui

--- client_ts/src/modules/cart/Cart.tsx
+++ client_ts/src/modules/cart/Cart.tsx
@@ -56,65 +56,87 @@ const CartWithItems = ({
 }) => {
 
   return (
-    <div className="container mb-5">
-      <h4 className="my-4 my-cart text-orange-500 font-semibold">My Cart</h4>
-      <div className="flex my-3 justify-between">
-        <h4 className="font-semibold">Summary</h4>
-        <h4 className="font-semibold mr-48">Cart</h4>
-      </div>
 
-      {/* Left Section */}
-      <div className="flex">
-        <div className="grid grid-cols-2 w-1/2">
-          <div className="col-span-1">
-            <h6>Total:</h6>
-            <h6>Promo Code:</h6>
-            <h6>Shipping:</h6>
-            <div className="my-3 line w-full"></div>
-            <h6>Subtotal:</h6>
+      <section className="h-screen bg-gray-100 py-12 sm:py-16 lg:py-20">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-center">
+            <h1 className="text-2xl font-semibold text-gray-900">Your Cart</h1>
           </div>
-          <div className="col-span-1">
-            <h6>{totalPrice}DH</h6>
-            <h6>MOBITECH</h6>
-            <h6>50DH</h6>
-            <div className="my-3 line w-60"></div>
-            <h6>{totalPrice + 50}DH</h6>
-          </div>
-        </div>
 
-        {/* Right Section */}
-        <div className="w-1/2">
-          <ul className="p-0">
-            {cartItems?.map((item: ProductItems, idx: number) => (
-              <li key={idx} className="list-none">
-                <div className="flex items-center justify-between cart-items p-4 bg-white">
-                  <img src={item.image} width="50px" height="50px" className="rounded-full" alt="icon" />
-                  <h6 className="mt-4">{item.name}</h6>
-                  <div className="flex mt-4">
-                    <button
-                      className="add-remove-btn"
-                      type="button"
-                      onClick={() => removeItem(item)}>
-                      -
-                    </button>
-                    <span className="cart-quantity mx-1">{item.quantity}</span>
-                    <button
-                      className="add-remove-btn"
-                      type="button"
-                      onClick={() => addToCart(item)}>
-                      +
-                    </button>
-                  </div>
-                  <p className="mt-4">Total Price</p>
-                  <h6 className="mt-4">{item.quantity! * item.price}DH</h6>
+          <div className="mx-auto mt-6 max-w-md md:mt-5">
+            <div className="rounded-3xl bg-white shadow-lg">
+              <div className="px-4 py-6 sm:px-8 sm:py-10">
+                <div className="flow-root">
+                  <ul className="-my-8">
+                  {cartItems?.map((item: ProductItems, idx: number) => (
+                    <li  key={idx} className="flex flex-col space-y-3 py-3 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
+                      <div className="shrink-0 relative">
+                        <span className="absolute top-1 left-1 flex h-6 w-6 items-center justify-center rounded-full border bg-white text-sm font-medium text-gray-500 shadow sm:-top-2 sm:-right-2">{item.quantity}</span>
+                        <img className="h-24 w-24 max-w-full rounded-lg object-cover" src={item.image} alt="" />
+                      </div>
+
+                      <div className="relative flex flex-1 flex-col justify-between">
+                        <div className="sm:col-gap-5 sm:grid sm:grid-cols-2">
+                          <div className="pr-8 sm:pr-5">
+                            <p className="text-base font-semibold text-gray-900">{item.name}</p>
+                          </div>
+
+                          <div className="mt-4 flex items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
+                            <p className="shrink-0 w-20 text-base font-semibold text-gray-900 sm:order-2 sm:ml-8 sm:text-right">{item.quantity! * item.price}.00 DH</p>
+                          </div>
+                        </div>
+
+                        <div className="items-center absolute top-0 right-0 flex sm:bottom-0 sm:top-auto">
+                          <button onClick={() => removeItem(item)} type="button" className="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+                          </svg>
+                          </button>
+
+                          <button type="button" className="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900">
+                          <p>{item.quantity}</p>
+                          </button>
+
+                          <button onClick={() => addToCart(item)} type="button" className="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                          </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </li>
+                    ))}
+                  </ul>
                 </div>
-                <div className="line"></div>
-              </li>
-            ))}
-          </ul>
+
+                <div className="mt-6 space-y-1 border-t border-b py-3">
+                  <div className="flex items-center justify-between">
+                    <p className="text-gray-400">Subtotal</p>
+                    <p className="text-lg font-semibold text-gray-900">{totalPrice} DH</p>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <p className="text-gray-400">Shipping</p>
+                    <p className="text-lg font-semibold text-gray-900">50 DH</p>
+                  </div>
+                </div>
+                <div className="mt-3 flex items-center justify-between">
+                  <p className="text-lg font-semibold text-gray-900">Total</p>
+                  <p className="text-2xl font-medium text-gray-900"> <span className="text-s text-gray-600">{totalPrice + 50} DH</span></p>
+                </div>
+
+                <div className="mt-4 text-center">
+                  <button type="button" className="group inline-flex w-full items-center justify-center rounded-md bg-sky-600 px-3 py-3 text-lg font-semibold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-800">
+                    Place Order
+                    <svg xmlns="http://www.w3.org/2000/svg" className="group-hover:ml-8 ml-4 h-6 w-6 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
   );
 };
 
